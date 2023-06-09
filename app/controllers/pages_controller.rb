288d13f80712b4ab3ead2fbfef4256946e1649fd
giorgi bobokhidze <gbobokhidze76@gmail.com>
2023-06-10 00:29:31 +0400

    added pagination

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
-    @people = Person.all
+    @people = Person.paginate(page: params[:page], per_page: 5)
   end
 end
