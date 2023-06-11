a717107bb3172957208e6bfb31d7c98ee4aeee77
giorgi bobokhidze <gbobokhidze76@gmail.com>
2023-06-11 12:52:38 +0400

    alerts for wrong parameters

--- app/services/csv_import_service.rb
+++ app/services/csv_import_service.rb
@@ -8,14 +8,38 @@ class CsvImportService
 
   def import
     @count = 0
-    # id, name, age
+    headers = nil # variable to store the headers
+
     CSV.foreach(@file.path, headers: true) do |row|
-      Person.create! row.to_hash
-      @count += 1
+      # Check if headers are present
+      if headers.nil?
+        headers = row.headers
+        # Check if headers match the expected parameters
+        unless headers == ['id', 'name', 'age']
+          raise StandardError, 'Your CSV file doesn\'t have correct parameters'
+        end
+      end
+
+      id = row['id']
+      next unless valid_auto_increment_id?(id)
+
+      begin
+        Person.create! row.to_hash
+        @count += 1
+      rescue ActiveRecord::RecordInvalid => e
+        # Handle record creation errors (e.g., validation errors)
+        raise StandardError, "Failed to import person: #{e.message}"
+      end
     end
   end
 
   def number_imported_with_last_run
     @count
+  end
+
+  private
+
+  def valid_auto_increment_id?(id)
+    id.present? && id.to_i.to_s == id
   end
 end
